46a5cf6d032c3640a23d0c5875c3839a52767d8c
AutomationTester-Pune <automation.tester.pune@gmail.com>
2024-06-23 18:56:31 +0530

    initial commit

---
+++ src/main/java/auto/tester/git_prac_1/App.java
@@ -0,0 +1,13 @@
+package auto.tester.git_prac_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
